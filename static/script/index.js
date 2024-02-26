1d6598cda53ae67ecc882f2988b0f21f022569ec
matthew3768 <s5624533@bournemouth.ac.uk>
2024-02-26 13:57:26 +0000

    progress button

--- static/script/index.js
+++ static/script/index.js
@@ -5,3 +5,9 @@ clicker.addEventListener('click', function () {
     count += 1
     console.log(count)
 })
+function updateProgress(progressId) {
+    var progressBar = document.getElementById(progressId);
+    var currentWidth = progressBar.style.width || '0%';
+    var newWidth = parseInt(currentWidth) + 10;
+    progressBar.style.width = newWidth > 100 ? '100%' : newWidth + '%';
+}
