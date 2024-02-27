cadbd704f16ee39eff7a6ca2f2995e854147e365
B-Turner5 <turnerbenjamin345@gmail.com>
2024-02-27 17:50:46 +0000

    Implemented Class Structure for Gameplay Elements

--- buildings.py
+++ buildings.py
@@ -1,4 +1,48 @@
+class GameStats:
+    def __init__(self):
+        ledBulbPoints = 1
+        ledBulbCost = 25
+        solarPanelPoints = 25
+        solarPanelCost = 500
+        boilerPoints = 2500
+        boilerCost = 15000
+    def calculate(self):
+        return None
+
 class Building:
-    def __init__():
+    def __init__(self):
         __ledBulbLevel = 0 ## Should Range from 1-20.
-        __
+        __solarPanelLevel = 0
+        __boilerUpgrade = False
+        __groundSourceHeatPumpsUpgrade = False
+        __insulationUpgrade = False
+
+    def getLEDBulbLevel(self):
+        return self.__ledBulbLevel
+    
+    def setLEDBulbLevel(self, level):
+        try:
+            if not (level <= 0 or level > 20):
+                self.__ledBulbLevel == level
+        except:
+            print("Error, " + str(level) + " is not a valid level.")
+
+    def getSolarPanelLevel(self):
+        return self.__ledBulbLevel
+    
+    def setSolarPanelLevel(self, level):
+        try:
+            if not (level <= 0 or level > 20):
+                self.__ledBulbLevel == level
+        except:
+            print("Error, " + str(level) + " is not a valid level.")
+
+    def getBoilerUpgradeStatus(self):
+        return self.__ledBulbLevel
+    
+    def setBoilerUpgradeStatus(self, level):
+        try:
+            if not (level <= 0 or level > 20):
+                self.__ledBulbLevel == level
+        except:
+            print("Error, " + str(level) + " is not a valid level.")
